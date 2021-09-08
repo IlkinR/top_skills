a4e9393d5b2b7fdb34c53d92fed8b0b09f6ce720
Ilkin Ramazanov <babalke44@gmail.com>
2021-09-08 22:21:51 +0400

    created first version of app

---
+++ top_skills.py
@@ -0,0 +1,45 @@
+import time
+from typing import Counter
+import requests
+from prettytable import PrettyTable
+
+HOME_URL = "https://api.hh.ru/vacancies"
+VACANCY_URL = "https://api.hh.ru/vacancies/"
+
+
+def main():
+    # 1. Collect all vacancy urls
+    payload = {"text": "Python", "page": 1, "per_page": 100}
+    response = requests.get(HOME_URL, params=payload)
+    vacancies = response.json().get("items")
+    vacancies_urls = [VACANCY_URL + v["id"] for v in vacancies]
+
+    # 2. Extract key skills from 1
+    all_skills = []
+
+    for pos, vacancy_url in enumerate(vacancies_urls, 1):
+        vacancy_response = requests.get(vacancy_url)
+        skills_list = vacancy_response.json().get("key_skills")
+        skills = [skill.get("name") for skill in skills_list]
+        all_skills += skills
+        print(f"{pos}) {vacancy_url} --- {skills}")
+
+    # with open("skills.txt", "w", encoding="utf-8") as file:
+    #     for skill in all_skills:
+    #         file.write(f"{skill}\n")
+
+    # 3. Find most frequent ones
+    # with open("skills.txt", "r", encoding="utf-8") as file:
+    #     skills = [row.strip("\n") for row in file]
+
+    skill_freqs = Counter(all_skills).most_common(15)
+    table = PrettyTable(field_names=["skill", "frequency"], title="Top Skills")
+    table.add_rows([[skill, frequency] for skill, frequency in skill_freqs])
+    print(table)
+
+
+start = time.perf_counter()
+main()
+end = time.perf_counter()
+run_time = end - start
+print(f"Script runs in {run_time} seconds")
